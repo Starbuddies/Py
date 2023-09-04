7dcc2545d023158c72911a53ff00617fee216ccf
Mrprohacker1 <142567751+Mrprohacker1@users.noreply.github.com>
2023-09-04 21:52:21 +0530

    Update 10( knapsack (dynamic) ).java

--- 10( knapsack (dynamic) ).java
+++ 10( knapsack (dynamic) ).java
@@ -1,61 +1,66 @@
-12) Hamiltonian cycle
-import java.util.*;
-Class Hamiltonian cycle
+
+Import java.util.Scanner;
+public class KnapsackDP
 {
-Private int adj[][],x[],n;
-public Hamiltonian cycle()
-{
-Scanner src = new Scanner(System.in);
-System.out.println("Enter the number of nodes");
-n=src.nextInt();
-x=new int[n]; x[0]=0;
-for (int i=1;i<n; i++) x[i]=-1;
-adj=new int[n][n];
-System.out.println("Enter the adjacency matrix");
-for (int i=0;i<n; i++)
-for (int j=0; j<n; j++) adj[i][j]=src.nextInt();
-}
-public void nextValue (int k)
-{
-int i=0;
-while(true)
-{
-x[k]=x[k]+1;
-if(x[k]==n)
-x[k]=-1;
-if (x[k]==-1)
-return;
-if (adj[x[k-1]][x[k]]==1)
-for (i=0; i<k; i++)
-if (x[i]==x[k])
-break;
-if(i==k)
-if (k<n-1 || k==n-1 &&adj[x[n-1]][0]==1)
-return;
-}
-}
-public void getHCycle(int k)
-{
-while(true)
-{
-nextValue(k);
-if(x[k]==-1)
-return; if(k==n-1)
-{
-System.out.println("\nSolution : ");
-for (int i=0; i<n; i++)
-System.out.print((x[i]+1)+" ");
-System.out.println(1);
-}
-Else getHCycle(k+1);
-}
-}
-}
-Class Hamiltonian cycleExp
-{
+static finalintMAX=20;//max.no.ofobjects static
+intw[]; // weights 0 ton-1
+static int p[]; // profits 0 ton-1
+static int n; // no. ofobjects
+static int M; // capacity of Knapsack
+static int V[][]; // DP solution process-table
+staticint Keep[][]; // to get objects in optimal solution
 public static void main(String args[])
 {
-Hamiltoniancycleobj=new Hamiltoniancycle();
-obj.getHCycle(1);
+w = new int[MAX];
+p = new int[MAX];
+V = new int [MAX][MAX];
+Keep = new int[MAX][MAX];
+intoptsoln;
+ReadObjects();
+for (int i= 0; i<= M;i++)
+V[0][i] = 0;
+for (int i= 0; i<= n;i++)
+V[i][0] =0;
+optsoln= Knapsack();
+System.out.println("Optimal solution= "+ optsoln);
+}
+staticint Knapsack()
+{
+int r; // remaining Knapsack capacity
+for (int i= 1; i<= n; i++)
+for (int j= 0; j<= M; j++)
+if((w[i] <= j) && (p[i] + V[i- 1][j- w[i]] > V[i- 1][j]))
+{else
+V[i][j] = p[i] + V[i- 1][j - w[i]];
+Keep[i][j] = 1;
+V[i][j] = V[i- 1][j];
+Keep[i][j] =0;
+//FindtheobjectsincludedintheKnapsack r
+=M;
+System.out.println("Items = ");
+for (int i= n; i> 0; i--) // start from Keep[n,M]
+if
+(Keep[i][r] == 1)
+{ System.out.println(i + "");
+r = r -w[i];
+}
+System.out.println();
+returnV[n][M];
+}
+static void ReadObjects()
+{
+Scanner scanner = new Scanner(System.in); System.out.println("Knapsack
+Problem - Dynamic Programming Solution: ");
+System.out.println("Enterthemaxcapacityofknapsack:");M=
+scanner.nextInt();
+System.out.println("Enternumberofobjects:");n=
+scanner.nextInt();
+System.out.println("Enter Weights:");for
+(inti= 1; i<= n;i++)
+w[i] = scanner.nextInt();
+System.out.println("Enter Profits:");for
+(inti= 1; i<= n; i++)
+p[i] = scanner.nextInt();
+scanner.close();
 }
 }

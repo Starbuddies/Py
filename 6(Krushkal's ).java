7ca9718203feba6ab7fa94a34e79b449d2291c15
Mrprohacker1 <142567751+Mrprohacker1@users.noreply.github.com>
2023-09-04 20:24:28 +0530

    Update 6(Krushkal's ).java

--- 6(Krushkal's ).java
+++ 6(Krushkal's ).java
@@ -1,5 +1,6 @@
-6) Krushkal's 
-import Java.util.Scanner;
+
+package kruskal;
+import java.util.Scanner;
 public class Kruskal {
 int parent[]=new int[10];
 int find(int m)
@@ -9,14 +10,14 @@ while(parent[p]!=0)
 p=parent[p];
 return p;
 }
-void union(inti,int j)
+void union(int i,int j)
 {
 if(i<j)
 parent[i]=j;
 else
 parent[j]=i;
 }
-voidkrkl(int[][]a, int n)
+void krkl(int[][]a, int n)
 {
 int u=0,v=0,min,k=0,i,j,sum=0;
 while(k<n-1)
@@ -42,17 +43,19 @@ k++;
 a[u][v]=a[v][u]=99;
 }
 System.out.println("The cost ofminimum spanning tree = "+sum);
+}
 public static void main(String[] args) {
 int a[ ][ ]=new int[10][10];
-inti,j;
+int i,j;
 System.out.println("Enter the number of vertices of the graph");
 Scanner sc=new Scanner(System.in);
 int n; n=sc.nextInt();
 System.out.println("Enter the wieghted matrix");
 for(i=1;i<=n;i++)
 for(j=1;j<=n;j++)
-a[i][j]=sc.nextInt(); kruskal k=new
-kruskal(); k.krkl(a,n);
+a[i][j]=sc.nextInt(); 
+Kruskal k=new Kruskal(); 
+k.krkl(a,n);
 sc.close();
 }
 }
